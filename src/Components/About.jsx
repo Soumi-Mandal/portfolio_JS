95110090b06da17abd018c78e435f56df7903fbb
Soumi Mandal <soumimandal806@gmail.com>
2024-12-09 21:38:00 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -44,7 +44,9 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "IT Skills: C/C++, SQL, HTML, CSS, Javascript, Java, Python";
+  "Academics: DSA, DBMS, OS, OOPs, Computer Networking";
+
 
 const About = () => {
   return (
